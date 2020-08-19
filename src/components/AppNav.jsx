6ffd8f4cd19ae8ff0707fb097dcca099987c6439
Mell Lessington <59120468+mjlessington@users.noreply.github.com>
2020-08-18 21:13:07 -0400

    add links

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import Login from './Login'
 import Logout from './Logout';
@@ -24,13 +26,38 @@ const useStyles = makeStyles((theme) => ({
 
     export default function ButtonAppBar() {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
 
     return (
         <div className={classes.root}>
         <AppBar position="static">
             <Toolbar>
             <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
+                <MenuIcon>
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                </Button>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}>
+                    <MenuItem onClick={handleClose} href= 'https://drive.google.com/drive/folders/1oD3sKUIJ_p_2V1PKR8S3zc2-kQhX0Rqd?usp=sharing' >Choir Docs</MenuItem>
+                    <MenuItem onClick={handleClose} href='https://www.musixmatch.com/'>Find Lyrics </MenuItem>
+                    <MenuItem onClick={handleClose}>Schedule</MenuItem>
+                </Menu>
+                </MenuIcon>
+
             </IconButton>
             <Typography variant="h6" className={classes.title}>
                 Choir Companion    
